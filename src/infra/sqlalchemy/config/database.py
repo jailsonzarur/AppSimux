7398cbe7d2b1ccb86662798801fa229d1f4585ce
jailsonzarur <jailsoncardoz123@gmail.com>
2024-05-20 21:08:22 -0300

    Banco de Dados Postgresql alterado.

--- src/infra/sqlalchemy/config/database.py
+++ src/infra/sqlalchemy/config/database.py
@@ -1,12 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./app_simux.db"
+load_dotenv('.env')
+
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./app_simux.db"
 #SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:pangare@localhost[3306]/app_blx.db"
-#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:pangare@localhost:5432/appblx"
+SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
